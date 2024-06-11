603bb8a82cde007a3833e6b37f8545c91ec252fe
Lucopecto <155958249+Lucopecto@users.noreply.github.com>
2024-06-11 18:50:31 +0200

    dash fonctionnel, mais traverse les murs

--- R202unity/Assets/Scripts/PlayerControl.cs
+++ R202unity/Assets/Scripts/PlayerControl.cs
@@ -1,21 +1,89 @@
 using UnityEngine;
 
+
+
+
+
 public class PlayerControl : MonoBehaviour
 {
+
+
+
     [SerializeField] private float _speed;
     [SerializeField] private float _jumpForce;
+    [SerializeField] private float _dashForce;
     [SerializeField] private Rigidbody _rb;
+
+    private bool dashRequested = false;
+    private bool dashRecharge = true;
 
     public float seuil = 0.1f;
 
- 
+
+    
     void Update()
     {
+        // Mouvement
         var vel = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _speed;
         vel.y = _rb.velocity.y;
         _rb.velocity = vel;
 
-        if (Input.GetKeyDown(KeyCode.Space) && EstAuSol()) _rb.AddForce(Vector3.up * _jumpForce);
+        // Saut
+        if (Input.GetKeyDown(KeyCode.Space) && EstAuSol())
+        {
+            _rb.AddForce(Vector3.up * _jumpForce);
+        }
+
+        // Rechargement du dash
+        if (EstAuSol())
+        {
+            dashRecharge = true;
+        }
+
+        // Demande de dash
+        if (Input.GetKeyDown(KeyCode.Return) && (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))
+        {
+            dashRequested = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (dashRequested)
+        {
+            dashRequested = false;
+            if (dashRecharge)
+            {
+                Dash();
+                dashRecharge = false;
+            }
+        }
+    }
+
+
+
+    void Dash()
+    {
+        Vector3 dashDirection = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            dashDirection -= transform.right;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            dashDirection += transform.right;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            dashDirection -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            dashDirection += transform.forward;
+        }
+
+        _rb.AddForce(dashDirection * _dashForce, ForceMode.Impulse);
     }
 
 
@@ -24,5 +92,9 @@ public class PlayerControl : MonoBehaviour
     {
         return Physics.Raycast(transform.position, -Vector3.up, GetComponent<Collider>().bounds.extents.y + seuil);
     }
+
+
+
+
 
 }
