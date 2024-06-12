214ee306d1ae0ca63841142e03fdda37e5affe82
Lucopecto <155958249+Lucopecto@users.noreply.github.com>
2024-06-12 08:33:32 +0200

    repush

--- R202unity/Assets/Scripts/PlayerControl.cs
+++ R202unity/Assets/Scripts/PlayerControl.cs
@@ -13,6 +13,7 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private float _jumpForce;
     [SerializeField] private float _dashForce;
     [SerializeField] private Rigidbody _rb;
+    [SerializeField] private TrailRenderer _tr;
 
     private bool dashRequested = false;
     private bool dashRecharge = true;
