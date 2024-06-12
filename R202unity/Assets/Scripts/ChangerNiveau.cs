22cbdd9b671c6656259afb515353cb29b30ade6f
Mel_3201 <150348629+melih0132@users.noreply.github.com>
2024-06-12 11:15:41 +0200

    victoire

--- R202unity/Assets/Scripts/ChangerNiveau.cs
+++ R202unity/Assets/Scripts/ChangerNiveau.cs
@@ -3,17 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class ChangerNiveau : MonoBehaviour
 {
     [SerializeField] private int _niveauActuel;
 
-
-
-    private void OnTriggerEnter(Collider other){
+    private void OnTriggerEnter(Collider other)
+    {
         if(other.tag == "ChangerNiveau")
         {
             SceneManager.LoadScene($"Niveau{_niveauActuel + 1}");
         }
+        else if(other.tag == "Quitter")
+        {
+            #if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+            Debug.Log("Jeu quitté");
+        }
     }
-
 }
