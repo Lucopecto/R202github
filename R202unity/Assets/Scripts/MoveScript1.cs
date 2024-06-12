49ad6f1bfea27ac0e76c6235580f46df1cffc10c
sftss <tasdemirsefer2005@gmail.com>
2024-06-12 16:54:02 +0200

    nv11

--- R202unity/Assets/Scripts/MoveScript1.cs
+++ R202unity/Assets/Scripts/MoveScript1.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class MoveScript1 : MonoBehaviour {
 
-    // private Vector3 normalizeDirection;
-
-	//public Transform target;
-	//public float speed = 5f;
-
-
-    // public Transform movingCubeDestination;
-    // public Transform firstPosition;
-   
-    // public float speed = 1.0f;
-    // public float distanceThreshold = 0.1f;
-
     public float speed = 2.0f; 
     public float distance = 5.0f;
     private Vector3 startPosition;
@@ -23,46 +11,27 @@ public class MoveScript1 : MonoBehaviour {
 
 	void Start()
 	{
-		//normalizeDirection = (target.position - transform.position).normalized;
         startPosition = transform.position;
 	}
 	
 	void Update()
 	{
+        float zOffset = Mathf.PingPong(Time.time * speed, distance);
+        transform.position = startPosition + new Vector3(0, 0, zOffset);
+        
+        // float zOffset = speed * Time.deltaTime;
+        // if (movingForward)
+        // {
+        //     transform.position -= new Vector3(0, 0, zOffset);
+        // }
+        // else
+        // {
+        //     transform.position += new Vector3(0, 0, zOffset);
+        // }
 
-    // float step = speed * Time.deltaTime;
-    //     if (whereDoIGo == true)
-    //     {
-    //         transform.position = Vector3.MoveTowards(transform.position, movingCubeDestination.position, step);
-    //         whereDoIGo = false;
-    //     }
-    //     else if (whereDoIGo == false)
-    //     {
-    //         transform.position = Vector3.MoveTowards(transform.position, firstPosition.position, step);
-    //         whereDoIGo = true;
-    //     }	// transform.position += normalizeDirection * speed * Time.deltaTime;
-    //     float step = speed * Time.deltaTime;
-    //     transform.position = Vector3.MoveTowards(transform.position, movingCubeDestination.position, step);
-
-    //     if(Vector3.Distance(movingCubeDestination.position,transform.position) <= distanceThreshold)
-    //     {
-    //         transform.position = new Vector3(1f,0f,0f);
-    //     }	
-
-        float zOffset = speed * Time.deltaTime;
-        if (movingForward)
-        {
-            transform.position -= new Vector3(0, 0, zOffset);
-        }
-        else
-        {
-            transform.position += new Vector3(0, 0, zOffset);
-        }
-
-        // Vérifier si le cube a atteint la distance maximale et inverser la direction
-        if (Vector3.Distance(startPosition, transform.position) >= distance)
-        {
-            movingForward = !movingForward;
-        }
+        // if (Vector3.Distance(startPosition, transform.position) >= distance)
+        // {
+        //     movingForward = !movingForward;
+        // }
     }
 }
