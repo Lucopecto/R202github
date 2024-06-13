fbbbb925625dc1f5f70f39b22b98ac6cfb021387
Lucopecto <155958249+Lucopecto@users.noreply.github.com>
2024-06-13 10:29:46 +0200

    interface monde1

--- R202unity/Assets/Scripts/WorldFinish.cs
+++ R202unity/Assets/Scripts/WorldFinish.cs
@@ -6,21 +6,18 @@ public class WorldFinish : MonoBehaviour
 {
     [SerializeField] public GameObject menuFinUI;
 
-    private float opaciteCanvas = 0f;
-    private bool mondeFini = false;
-
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        menuFinUI.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "ChangerMonde")
         {
-            NiveauSuivant();
+            menuFinUI.SetActive(true);
         }
     }
 
